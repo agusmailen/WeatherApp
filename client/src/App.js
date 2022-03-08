f087f447e751276348e5c7033771fb20f8375eb6
Agus Mailen <marrapodiagustina@gmail.com>
2022-03-08 10:40:31 -0300

    restart app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-            <Route exact path='/hola' element={<Home />}/>
-        </Routes>
-    </BrowserRouter>
-  )
-};
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
