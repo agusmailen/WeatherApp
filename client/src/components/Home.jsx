86edbc0afe75beb3c34dc31aa381d891ab267475
Agus Mailen <marrapodiagustina@gmail.com>
2022-03-08 21:43:19 -0300

    identation in home fixed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,13 +6,13 @@ import '../assets/styles/home.css';
 const Home = () => {
 	return (
 		<div className="container-home">
-				<div className="container-form">
-					<Register />
-				</div>
-				<div className="line" />
-				<div className="container-form">
-					<Login />
-				</div>
+			<div className="container-form">
+				<Register />
+			</div>
+			<div className="line" />
+			<div className="container-form">
+				<Login />
+			</div>
 		</div>
 	);
 };
