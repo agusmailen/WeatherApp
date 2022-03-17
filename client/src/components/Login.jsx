9a661cdfa49681a81155c3350b9f247da76ea30e
Agus Mailen <marrapodiagustina@gmail.com>
2022-03-16 22:49:32 -0300

    form login register validations / styles btn

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,25 @@
 import react from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+	const { register, handleSubmit, formState: { errors } } = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<>
 			<h2 className='title-form'>Inicia sesion</h2>
-			<form action=''>
-				<TextField id="standard-basic" label="Contraseña" variant="standard" />
-				<TextField id="standard-basic" label="Nombre" variant="standard" />
-				<Button variant="contained">Comenzar</Button>
+			<form action='' onSubmit={handleSubmit(onSubmit)}>
+				<TextField id='standard-basic' label='Email' variant="standard" mane='email' {...register('email', { required: true })}/>
+				{errors.email?.type === 'required' && <p className='form-error'>Ingrese su email</p>}
+				<TextField id='standard-basic' label='Contraseña' variant="standard" mane='nombre' {...register('password', { required: true })}/>
+				{errors.password?.type === 'required' && <p className='form-error'>Ingrese una contraseña</p>}
+				<Button variant="contained" type='submit'>Comenzar</Button>
 			</form>
 		</>
 	);
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,16 +1,28 @@
 import react from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+
+	const { register, handleSubmit, formState: { errors } } = useForm();
+	
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<>
 			<h2 className='title-form'>Registrate</h2>
-			<form action=''>
-				<TextField id="standard-basic" label="Nombre" variant="standard" />
-				<TextField id="standard-basic" label="email" variant="standard" />
-				<TextField id="standard-basic" label="constraseña" variant="standard" />
-				<Button variant="contained">Registrarme</Button>
+			<form action='' onSubmit={handleSubmit(onSubmit)}>
+				<TextField id='standard-basic' label='Nombre' variant='standard' {...register('name', { required: true })}/>
+				{errors.name?.type === 'required' && <p className='form-error'>Ingrese su nombre</p>}
+				<TextField id='standard-basic' label='email' variant='standard' {...register('email', { required: true, pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ })}/>
+				{errors.email?.type === 'required' && <p className='form-error'>Ingrese su nombre</p>}
+				{errors.email?.type === 'pattern' && <p className='form-error'>Ingrese un correo válido example@xxxx.com</p>}
+				<TextField id='standard-basic' label='constraseña' variant='standard' {...register('password', { required: true })}/>
+				{errors.password?.type === 'required' && <p className='form-error'>Ingrese una contraseña</p>}
+				<Button variant='contained' type='submit'>Registrarme</Button>
 			</form>
 		</>
 	);
