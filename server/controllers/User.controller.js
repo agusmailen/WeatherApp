65ce22c899069d0b23c0a250fb387e0954c4a7f4
Agus Mailen <marrapodiagustina@gmail.com>
2022-09-20 22:45:15 -0300

    amend commit/ fix api key in config

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -9,17 +9,39 @@ class UserController {
 		.then( response => {
 			if (!response) return res.json({
 				status: 430,
-				message: 'Ocurrió un error. El usuario no pudo ser registrado'
+				message: 'Ocurrió un error. Ya existe un usuario con este email'
 			});
 			return res.json({
 				status: 200,
-				message: 'El usuario fue creado con exito'
+				message: 'El usuario fue creado con exito',
+				response
 			})
 		})
 		.catch(err => {
 			return res.json ({ status: 400, error: err.message, message: 'Ocurrió un error. El usuario no pudo ser registrado'});
 		});
 	};
+
+
+	login(req, res) {
+		const payload = req.body;
+		console.log('desde constrolador', payload)
+		User.login(payload)
+		.then( response => {
+			if (!response) return res.json({
+				status: 430,
+				message: 'Ocurrió un error. Credenciales incorrectas'
+			});
+			return res.json({
+				status: 200,
+				message: 'Logueo Exitoso',
+				response
+			})
+		})
+		.catch(err => {
+			return res.json ({ status: 400, error: err.message, message: 'Ocurrió un error. El usuario no pudo ser logueado'});
+		})
+	}
 
 }
 
--- server/services/User.service.js
+++ server/services/User.service.js
@@ -1,4 +1,5 @@
 const pool = require('../database/database');
+const jwt = require('../utils/jwt');
 //crear usuario
 
 //loguear usuario
@@ -9,8 +10,18 @@ class UserService {
 		const existUser = await pool.query('SELECT * FROM users WHERE useremail = ?', [useremail]);
 		if(existUser.length) return false;
 		const payload = { username, useremail, userpassword };
+		payload.userpassword = jwt.token(payload.userpassword);
 		await pool.query('INSERT INTO users set ?', [payload]);
-		return payload;
+		return payload.username;
+	}
+
+
+	login = async ({email, password}) => {
+		const existUser = await pool.query('SELECT * FROM users WHERE useremail = ?', [email]);
+		if(!existUser.length) return false;
+		const payload = { email, password };
+		const existPassword = existUser[0].userpassword;
+		return jwt.decoded(existPassword).data === payload.password ? { "id": existUser[0].id, "email": payload.email, "name": existUser[0].username, "verify": existUser[0].verificated} : false;
 	}
 
 }
